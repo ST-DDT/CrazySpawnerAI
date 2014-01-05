0760953f8e108c974635a9c9a5f7fbac79fea5a3
ST-DDT <ST-DDT@gmx.de>
2014-01-05 12:55:50 +0100

    Finished implementation of RandomLookaroungGoal

--- src/de/st_ddt/crazyspawner/entities/properties/ai/action/builder/impl/RandomLookaroundGoalBuilder.java
+++ src/de/st_ddt/crazyspawner/entities/properties/ai/action/builder/impl/RandomLookaroundGoalBuilder.java
@@ -1,6 +1,33 @@
 package de.st_ddt.crazyspawner.entities.properties.ai.action.builder.impl;
 
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Creature;
 
-public class RandomLookaroundGoalBuilder
+import de.st_ddt.crazyspawner.entities.properties.ai.action.goals.Goal;
+import de.st_ddt.crazyspawner.entities.properties.ai.action.goals.impl.RandomLookaroundGoal;
+
+public class RandomLookaroundGoalBuilder extends BasicGoalBuilder
 {
+
+	public RandomLookaroundGoalBuilder()
+	{
+		super();
+	}
+
+	public RandomLookaroundGoalBuilder(final ConfigurationSection config)
+	{
+		super(config);
+	}
+
+	@Override
+	public String getType()
+	{
+		return "RandomLookaround";
+	}
+
+	@Override
+	public Goal build(final Creature entity)
+	{
+		return new RandomLookaroundGoal(entity);
+	}
 }
