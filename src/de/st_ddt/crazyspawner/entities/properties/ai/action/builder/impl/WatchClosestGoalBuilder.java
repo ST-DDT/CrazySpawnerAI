1bfb8b87ec50effd08ab8e6e352da784b91e8345
ST-DDT <ST-DDT@gmx.de>
2014-01-05 12:58:27 +0100

    Fixed loading and saving of WatchClosestGoalBuilder

--- src/de/st_ddt/crazyspawner/entities/properties/ai/action/builder/impl/WatchClosestGoalBuilder.java
+++ src/de/st_ddt/crazyspawner/entities/properties/ai/action/builder/impl/WatchClosestGoalBuilder.java
@@ -28,17 +28,17 @@ public class WatchClosestGoalBuilder extends BasicGoalBuilder
 	public WatchClosestGoalBuilder(final ConfigurationSection config)
 	{
 		super(config);
-		Condition watchedCondition = null;
+		Condition watchCondition = null;
 		try
 		{
-			watchedCondition = BasicCondition.load(config.getConfigurationSection("startCondition"));
+			watchCondition = BasicCondition.load(config.getConfigurationSection("watchCondition"));
 		}
 		catch (final Exception e)
 		{
 			System.err.println(config.getName() + "'s watch condition was corrupted/invalid and has been removed!");
-			watchedCondition = new Condition_TRUE();
+			watchCondition = new Condition_TRUE();
 		}
-		this.watchCondition = watchedCondition;
+		this.watchCondition = watchCondition;
 		this.yawRotationSpeed = Math.max(config.getDouble("yawRotationSpeed", Navigation.DEFAULTROTATIONSPEED), 0);
 		this.pitchRotationSpeed = Math.max(config.getDouble("pitchRotationSpeed", Navigation.DEFAULTROTATIONSPEED), 0);
 	}
@@ -53,5 +53,14 @@ public class WatchClosestGoalBuilder extends BasicGoalBuilder
 	public Goal build(final Creature entity)
 	{
 		return new WatchClosestGoal(entity, watchCondition, 0, 0);
+	}
+
+	@Override
+	public void save(final ConfigurationSection config, final String path)
+	{
+		super.save(config, path);
+		watchCondition.save(config, path + "watchCondition.");
+		config.set(path + "yawRotationSpeed", yawRotationSpeed);
+		config.set(path + "pitchRotationSpeed", pitchRotationSpeed);
 	}
 }
