672f47e520460b09bd9523ae4204c1229aa42bc1
ST-DDT <ST-DDT@gmx.de>
2014-07-02 22:07:27 +0200

    Updated to latest CrazySpawner version

--- src/main/java/de/st_ddt/crazyspawner/entities/properties/ai/CreatureActionSetProperty.java
+++ src/main/java/de/st_ddt/crazyspawner/entities/properties/ai/CreatureActionSetProperty.java
@@ -77,7 +77,7 @@ public class CreatureActionSetProperty extends BasicProperty
 	}
 
 	@Override
-	public boolean isApplicable(final Class<? extends Entity> clazz)
+	public boolean isApplicable(final Class<?> clazz)
 	{
 		return Creature.class.isAssignableFrom(clazz);
 	}
